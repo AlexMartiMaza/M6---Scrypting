fced11f75ab8fbd063d244194b85e1111382032d
Alex Marti Maza <158284832+AlexMartiMaza@users.noreply.github.com>
2024-02-01 10:32:52 +0100

    Create S4 - Comillas arguments.sh

---
+++ P1/S4 - Comillas arguments.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Els arguments rebuts:"
+echo "\$*: $*"
+echo "\$* (entre comillas): \"$*\""
+echo "\$@: $@"
+echo "\$@ (entre comillas): \"$@\""
+
+echo "\$* mostra tots els arguments com a una sola paraula"
+echo "\$@ mostra cada argument com una paraula "
