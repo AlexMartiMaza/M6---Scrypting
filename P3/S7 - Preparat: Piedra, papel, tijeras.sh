58e213211fe5c0270105750efb3ddcb4f89d5450
Alex Marti Maza <158284832+AlexMartiMaza@users.noreply.github.com>
2024-02-08 10:53:28 +0100

    Update S7 - Preparat: Piedra, papel, tijeras.sh

--- P3/S7 - Preparat: Piedra, papel, tijeras.sh
+++ P3/S7 - Preparat: Piedra, papel, tijeras.sh
@@ -2,38 +2,34 @@
 
 while true; do
     clear
-    echo "¡Bienvenido al juego de piedra, papel o tijeras!"
-    echo "Elige tu opción (piedra, papel o tijeras):"
+    echo "Welcome to Piedra, Papel o Tijeras"
+    echo "Elige: (piedra, papel o tijeras):"
     read -r eleccion_usuario
 
-    # Verificar la elección del usuario
     if [ "$eleccion_usuario" = "piedra" ] || [ "$eleccion_usuario" = "papel" ] || [ "$eleccion_usuario" = "tijeras" ]; then
-        # Generar la elección aleatoria de la máquina
         opciones=("piedra" "papel" "tijeras")
         eleccion_maquina=${opciones[$((RANDOM % 3))]}
         
-        # Determinar el resultado del juego
         if [ "$eleccion_usuario" = "$eleccion_maquina" ]; then
             resultado="Empate"
         elif [ "$eleccion_usuario" = "piedra" ] && [ "$eleccion_maquina" = "tijeras" ] ||
              [ "$eleccion_usuario" = "papel" ] && [ "$eleccion_maquina" = "piedra" ] ||
              [ "$eleccion_usuario" = "tijeras" ] && [ "$eleccion_maquina" = "papel" ]; then
-            resultado="¡Felicidades! ¡Has ganado!"
+            resultado="WINNNEEEER!!"
         else
-            resultado="La máquina gana"
+            resultado="LOOOSEEEER!!!"
         fi
 
-        # Mostrar el resultado del juego
-        echo "Tu elección: $eleccion_usuario"
-        echo "Elección de la máquina: $eleccion_maquina"
+
+        echo "Tu eleccion: $eleccion_usuario"
+        echo "Elección de la maquina: $eleccion_maquina"
         echo "Resultado: $resultado"
 
-        # Preguntar al usuario si quiere seguir jugando
-        read -p "¿Quieres seguir jugando? (s/n): " continuar
+        read -p "Seguir jugando? (s/n): " continuar
         if [ "$continuar" != "s" ]; then
             break
         fi
     else
-        echo "Opción no válida. Por favor, elige entre piedra, papel o tijeras."
+        echo "ERROR: piedra, papel o tijeras."
     fi
 done
